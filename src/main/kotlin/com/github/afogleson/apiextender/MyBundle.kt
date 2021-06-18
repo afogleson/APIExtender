2612d861737c3ab372704750607ad3f423d43dba
GitHub Action <action@github.com>
2021-06-18 00:59:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/afogleson/apiextender/MyBundle.kt
+++ src/main/kotlin/com/github/afogleson/apiextender/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.afogleson.apiextender
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/afogleson/apiextender/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/afogleson/apiextender/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.afogleson.apiextender.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.afogleson.apiextender.MyBundle
 
 class MyApplicationService {
 
